fc71113037d54d52ecee0fd3d88362005095d799
jbrasko <jbrasko0508@gmail.com>
2021-02-17 21:26:11 -0600

    add controller actions

--- app/controllers/presets_controller.rb
+++ app/controllers/presets_controller.rb
@@ -1,2 +1,34 @@
 class PresetsController < ApplicationController
+
+    def create
+        synth = Synth.find_by(id: 1)
+        preset = synth.presets.build(preset_params)
+        if preset.save
+            render json: preset
+        end
+    end
+
+    def index
+        presets = Synth.find_by(id: params[:synth_id]).presets
+        render json: presets
+    end
+
+    private
+    def preset_params
+        params.require(:preset).permit(
+            :name,
+            :wave_type,
+            :filter_dial,
+            :attack_value,
+            :decay_value,
+            :sustain_value,
+            :release_value,
+            :reverb_dial,
+            :delay_dial,
+            :trem_switch,
+            :trem_frequency,
+            :chorus_switch
+        )
+    end
 end
+
--- app/controllers/synths_controller.rb
+++ app/controllers/synths_controller.rb
@@ -1,2 +1,7 @@
 class SynthsController < ApplicationController
+
+    def index
+        synths = Synth.all
+        render json: synths
+    end
 end
